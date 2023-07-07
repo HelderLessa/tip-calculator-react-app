b0622a2756472850db60382701e2a68a3be6c6e4
Helder Lessa <116907887+HelderLessa@users.noreply.github.com>
2023-07-07 16:27:27 -0300

    Update Bill.js

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -1,12 +1,12 @@
 export default function Bill({ bill, onBill }) {
   return (
-    <h3>
-      <span>How much was the bill?</span>
+    <div>
+      <label>How much was the bill?</label>
       <input
         type="number"
         value={bill}
         onChange={(e) => onBill(+e.target.value)}
       />
-    </h3>
+    </div>
   );
 }
